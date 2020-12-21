3a2f2829fe59dcb45fbde4f33f64cbe28dc023d6
Qiangjun Ran <ranqiangjun@gmail.com>
2020-12-21 16:59:12 +0800

    Add Google Analytics

---
+++ ga.js
@@ -0,0 +1,9 @@
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BVLJR0H5W0"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BVLJR0H5W0');
+    </script>
